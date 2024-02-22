0b58ac96a320f842e328278db179c79991a0b601
Thomas Bell <thomasjbell065@gmail.com>
2024-02-21 21:20:20 -0500

    simplified onChange and onClick anonymous function

--- src/components/book/BookList.jsx
+++ src/components/book/BookList.jsx
@@ -66,81 +66,24 @@ const Book = props => {
 };
 
 const Modal = () => {
-    const handleFormSubmit = e => {
-        e.preventDefault();
-        console.log('handle form Submit');
-    };
-    const handleFormInput = e => {
-        console.log(e.target.name);
-        console.log(e.target.value);
-        console.log('handle form input');
-    };
-    const handleButtonClick = () => {
-        alert('handle button click');
-        // <button
-        //     type='button'
-        //     className='btn btn-primary'
-        //     data-bs-toggle='modal'
-        //     data-bs-target='#exampleModal'
-        // >
-        //     Click Me
-        // </button>;
-    };
     return (
-        <>
-            {/* <!-- Modal --> */}
-            {/* <div
-                className='modal fade'
-                id='exampleModal'
-                tabindex='-1'
-                aria-labelledby='exampleModalLabel'
-                aria-hidden='true'
-            >
-                <div className='modal-dialog'>
-                    <div className='modal-content'>
-                        <div className='modal-header'>
-                            <h1
-                                className='modal-title fs-5'
-                                id='exampleModalLabel'
-                            >
-                                Modal title
-                            </h1>
-                            <button
-                                type='button'
-                                className='btn-close'
-                                data-bs-dismiss='modal'
-                                aria-label='Close'
-                            ></button>
-                        </div>
-                        <div className='modal-body'>...</div>
-                        <div className='modal-footer'>
-                            <button
-                                type='button'
-                                className='btn btn-secondary'
-                                data-bs-dismiss='modal'
-                            >
-                                Close
-                            </button>
-                            <button type='button' className='btn btn-primary'>
-                                Save changes
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+        <div>
+            <form>
+                <h2>Typical Form</h2>
+                <input
+                    type='text'
+                    name='example'
+                    style={{ margin: '1rem 0' }}
+                    onChange={(e) => console.log(e.target.value)}
+                />
+                <button type='submit' onClick={() => console.log('form submitted')}>submit form</button>
+            </form>
             <div>
-                <form onSubmit={handleFormSubmit}>
-                    <h2>Typical Form</h2>
-                    <input
-                        type='text'
-                        name='example'
-                        onChange={handleFormInput}
-                        style={{ margin: '1rem 0' }}
-                    />
-                </form>
-                <button onClick={handleButtonClick}>click me</button>
+                <button onClick={() => alert('anonymous call back click')}>
+                    click me
+                </button>
             </div>
-        </>
+        </div>
     );
 };
 
